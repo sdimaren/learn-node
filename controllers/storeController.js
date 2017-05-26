af701a549b4c720b5d7e4ca63756d31add2bc7eb
Santiago Dimaren <sdimaren1@gmail.com>
2017-05-26 19:56:23 -0400

    Added mixins

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,12 @@
 exports.homePage = (req, res) => {
 	res.render('index');
 };
+
+exports.addStore = (req, res) => {
+	res.render('editStore', {title: 'Add Store'});
+};
+
+exports.createStore = (req, res) => {
+	console.log(req.body)
+	res.json(req.body);
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
