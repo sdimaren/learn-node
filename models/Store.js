60e2ee3b5ad7d3b21778c31a4d4201569f3954fe
Santiago Dimaren <sdimaren1@gmail.com>
2017-06-04 15:41:11 -0400

    Added pre save hooks to make unique slugs

--- models/Store.js
+++ models/Store.js
@@ -15,32 +15,38 @@ const storeSchema = new mongoose.Schema({
   },
   tags: [String],
   created: {
-  	type: Date,
-  	default: Date.now
+    type: Date,
+    default: Date.now
   },
   location: {
-  	type: {
-  		type: String,
-  		default: 'Point'
-  	},
-  	coordinates: [{
-  		type: Number,
-  		required: 'You must supply coordinates!'
-  	}],
-  	address: {
-  		type: String,
-  		required: 'You must supply an address!'
-  	}
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
   },
   photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from running
   }
   this.slug = slug(this.name);
+  // find other stores that have a slug of wes, wes-1, wes-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
   // TODO make more resiliant so slugs are unique
 });
