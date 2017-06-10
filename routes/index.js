50fd3bb7888f95a276d2c5a372589f0899c859f9
Santiago Dimaren <sdimaren1@gmail.com>
2017-06-10 13:01:05 -0400

    added registered users to db

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -30,8 +31,12 @@ router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
 
 // 1. validate the registration data
-// 2.register the user
+// 2. register the user
 // 3. we need to log them in
-router.post('/register', userController.validateRegister)
+router.post('/register', 
+	userController.validateRegister,
+	userController.register,
+	authController.login
+);
 
 module.exports = router;
