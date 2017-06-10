b87d4aed74a43377ad1c2b1ed00231cc2a13e817
Santiago Dimaren <sdimaren1@gmail.com>
2017-06-10 14:55:40 -0400

    added virtual fields, login-logout middleware and routes protection

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
 	successRedirect: '/',
 	successFlash: 'You now logged in!'
 });
+
+exports.logout = (req, res) => {
+	req.logout();
+	req.flash('success', 'You are now logged out!');
+	res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+	// first check if the user is authenticated
+	if(req.isAuthenticated()) {
+		next();
+		return;
+	}
+	req.flash('error', "You must be logged in!");
+	res.redirect('/login');
+}
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 
 router.post('/add', 
 	storeController.upload, 
@@ -28,6 +28,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1. validate the registration data
@@ -39,4 +40,5 @@ router.post('/register',
 	authController.login
 );
 
+router.get('/logout', authController.logout);
 module.exports = router;
