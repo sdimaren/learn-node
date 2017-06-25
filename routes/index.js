b9103266ca9f924776c5e56d23a7979a0603e5c6
Santiago Dimaren <sdimaren1@gmail.com>
2017-06-24 22:55:17 -0400

    added display for hearted stores

--- routes/index.js
+++ routes/index.js
@@ -53,6 +53,7 @@ router.post('/account/reset/:token',
 );
 
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 /*
  API
 */
